735fcc5f2948d5a11616eb7d0bd121bd5a3bfab8
hyo <vlsjt3@naver.com>
2021-03-12 21:52:03 +0900

    State Study

--- src/App.js
+++ src/App.js
@@ -1,77 +1,46 @@
-import React from "react"
-import PropTypes from "prop-types"
+import React from "react";
 
-const foodILike = [
-  {
-    id: 1,
-    name: "kimchi",
-    image:
-      "http://m.gochoochongak.com/web/product/big/201810/1cf1eb74d9cbb972ee0fd10d7f25e49c.jpg",
-    rating: 5,
-  },
-  {
-    id: 2,
-    name: "ramen",
-    image:
-      "https://rimage.gnst.jp/livejapan.com/public/article/detail/a/00/03/a0003253/img/ko/a0003253_parts_5ce7b2d079944.jpg?20210108193549&q=80&rw=686&rh=490",
-    rating: 4.9,
-  },
-  {
-    id: 3,
-    name: "samgiopsal",
-    image:
-      "https://newsimg.hankookilbo.com/cms/articlerelease/2020/06/11/202006111362061920_7.jpg",
-    rating: 4.8,
-  },
-  {
-    id: 4,
-    name: "chukumi",
-    image:
-      "https://recipe1.ezmember.co.kr/cache/recipe/2019/03/05/52d2be99c015378a75c9db81362422c71.jpg",
-    rating: 4.7,
-  },
-]
+//function component는 뭔가를 return 함.
+//class component는 class지만 react component로 부터 확장되고 render method를 가질 수 있음.
+//react는 자동적으로 class component의 render method를 실행함.
+class App extends React.Component {
+  state = {
+    count: 0,
+  };
 
-// function renderFood(dish) {
-//   console.log(dish);
-//   return <Food name={dish.name} pic={dish.image} />
-// }
+  // same as function() {}
+  add = () => {
+    this.setState(current => ({ count: current.count + 1 })); //setState()는 state의 값을 변경할 수 있고, 동시에 render도 새로 호출한다.
+  };
+  minus = () => {
+    this.setState(current => ({ count: current.count - 1 }));
+  };
 
-//component 생성
-function Food({ name, pic, rating }) {
-  //props.fav == { fav }
-  //console.log(prop.fav);
-  return (
-    <div>
-      <h2>I like {name}</h2>
-      <h4>{rating}/5.0</h4>
-      <img src={pic} alt={name} />
-    </div>
-  )
+  componentDidMount() {
+    //render 후
+    console.log("Component rendered");
+  }
+
+  componentDidUpdate() {
+    //render 후 업데이트가 일어날 시
+    console.log("I just updated");
+  }
+
+  componentWillUnmount() {
+    //component를 떠날 때
+    console.log("GoodBye.");
+  }
+
+  render() {
+    console.log("I'm rendering");
+    return (
+      <div>
+        <h1>The Number is: {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
+  }
 }
 
-//전달 받아야하는 prop을 체크할 수 있음 (type, required)
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  pic: PropTypes.string.isRequired,
-  rating: PropTypes.number,
-}
-
-// <Food /> -> JSX  JSX는 react의 개념으로 javascript + HTML임
-// component는 대문자로 시작해야하고, component로 정보를 보낼 수 있음 (ex. fav="kimchi")
-function App() {
-  return (
-    <div>
-      {foodILike.map(dish => (
-        <Food
-          key={dish.id}
-          name={dish.name}
-          pic={dish.image}
-          rating={dish.rating}
-        />
-      ))}
-    </div>
-  )
-}
-
-export default App
+export default App;
